80098b317e4d9085beb78eddd84971580f318fa9
aiyanzielf <aiyanzielf@gmail.com>
2020-09-13 10:49:47 +0800

    这是VPS重新部署后的第二次提交

---
+++ Makefile
@@ -0,0 +1,9 @@
+CC=gcc
+CXX=g++
+RANLIB=ranlib
+LD=ld
+LDXX=g++
+AR=ar
+STRIP=strip
+
+SRC=
--- test.cpp
+++ test.cpp
@@ -1,9 +1,27 @@
 #include <cstdio>
 #include <vector>
+#include <map>
 
 int main(int argc, char const *argv[])
 {
     printf("ggggggggggg");
     std::vector<int> bb;
+    std::map<int, int> cc;
+
+    cc.emplace(1, 1);
     return 0;
 }
+
+class monitor
+{
+public:
+    monitor() = default;
+    monitor(const monitor &) = default;
+    monitor(monitor &&) = default;
+    monitor &operator=(const monitor &) = default;
+    monitor &operator=(monitor &&) = default;
+
+private:
+};
+
+
